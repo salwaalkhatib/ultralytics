35ae947d46fd6f8241a621cd2cb9a10e0d16820e
Jameel Hassan <you@example.comjhassan.tck14@gmail.com>
2023-03-19 15:28:28 +0400

    minor update

--- inference.py
+++ inference.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
-PROJECT = 'init'
+PROJECT = 'experiments/init'
 EPOCHS = 50
 EXPERIMENT = 'small_ep' + str(EPOCHS)
 
 model = YOLO("yolov8s.pt")
-model.train(data="coco128.yaml", epochs=EPOCHS, save_period=9, batch=8, project=PROJECT, name=EXPERIMENT)
+model.train(data="isaid.yaml", epochs=EPOCHS, save_period=9, batch=8, project=PROJECT, name=EXPERIMENT)
